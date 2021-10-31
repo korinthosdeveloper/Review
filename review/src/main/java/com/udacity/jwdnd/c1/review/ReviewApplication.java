4b63b0a13a1b37dd4e6d509d92bf8fcbf193edcc
korinthosdeveloper <tasos.tsamantas.tt@gmail.com>
2021-10-31 12:19:56 +0200

    first up!

---
+++ review/src/main/java/com/udacity/jwdnd/c1/review/ReviewApplication.java
@@ -0,0 +1,23 @@
+package com.udacity.jwdnd.c1.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@SpringBootApplication
+
+public class ReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewApplication.class, args);
+	}
+
+
+
+
+
+}
